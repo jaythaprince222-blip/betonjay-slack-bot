e0406de6797438b2b7378e3f141bf47d538527fe
jaythaprince222-blip <jaythaprince222@gmail.com>
2026-05-13 20:43:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { WebClient } from "@slack/web-api";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const client = new WebClient(process.env.SLACK_BOT_TOKEN);
+
 app.post("/slack/commands", async (req, res) => {
   const { channel_id, user_name } = req.body;
 
@@ -7,4 +18,7 @@ app.post("/slack/commands", async (req, res) => {
   });
 
   res.status(200).send();
+});
+app.listen(port, () => {
+  console.log(`⚡ BetOnJay Slack bot running on port ${port}`);
 });
