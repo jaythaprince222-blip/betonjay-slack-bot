10d211aa7c5ff92cc8e50eb5768b980db686b8db
jaythaprince222-blip <jaythaprince222@gmail.com>
2026-05-13 19:47:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const { App } = require('@slack/bolt');
+app.post("/slack/commands", async (req, res) => {
+  const { channel_id, user_name } = req.body;
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  await client.chat.postMessage({
+    channel: channel_id,
+    text: `Hey ${user_name}, your BetOnJay bot is live!`
+  });
+
+  res.status(200).send();
 });
-
-app.event('app_home_opened', async ({ event, say }) => {
-  await say('BetOnJay bot is live!');
-});
-
-(async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log('⚡️ BetOnJay Slack bot running on port 3000');
-})();
